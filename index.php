Noziro/MediaTracker

commit a1356f78bad0b2d593cc1d9f5433e55e75f79c4b

(Plumbing) Clean-up URL/file code handling

index.php
@@ -1,6 +1,6 @@
 <?php
 
-# REQUIRED SETUP
+// SETUP
 
 # Defines basic PATH for easy use across both client & server
 define("PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
@@ -8,80 +8,62 @@ define("PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
 # Includes various important variables and functions
 include PATH."server/server.php";
 
+# the URL as displayed in the client browse
+DEFINE("URL", [
+	'PATH_STRING' => strtok($_SERVER["REQUEST_URI"], '?'),
+	'PATH_ARRAY' => remove_empties(explode('/', strtok($_SERVER["REQUEST_URI"], '?')))
+]);
 
 
-# INDEX-SPECIFIC SETUP
 
-# the URL as displayed in the client browser
-$external_url = [
-	'path_string' => strtok($_SERVER["REQUEST_URI"], '?'),
-	'path_array' => array_filter(explode('/', strtok($_SERVER["REQUEST_URI"], '?')))
-];
+// INDEX-SPECIFIC SETUP
 
-# Strips GET variables off the end and returns only the base URL
-$url = $external_url['path_string'];
+// Determine which file to load as the main page content
 
-# Check for various conditions related to the URL and make usable later in code
-
-if( $url == '/' && !$has_session ){
-	$url = 'about';
+if( empty(URL['PATH_ARRAY']) ){
+	$file = $has_session ? 'index' : 'about';
+	$page_title = $has_session ? 'Dashboard' : 'Track Your Collections!';
 }
-elseif( $url == '/' && $has_session ){
-	$url = 'index';
+elseif( URL['PATH_ARRAY'][0] === 'collection' ){
+	$file = 'collection';
+}
+elseif( URL['PATH_ARRAY'][0] === 'register' ){
+	$file = 'login';
+}
+// TODO: this is rather garbage. Better to redirect all number error codes in .htaccess to error.php or something
+elseif( in_array(URL['PATH_ARRAY'][0], ['403', '404', '500']) ){
+	$file = 'error';
 }
 else {
-	// strips the / off the beginning
-	$url = substr($url, 1);
+	# replaces all slashes with ^. This matches the file names in "views" folder
+	$file = str_replace('/', '^', substr(URL['PATH_STRING'], 1));
 }
 
-// replaces all slashes with ^. This matches the file names in "views" folder
-$url = str_replace('/', '^', $url);
+// Set page title.
+# Defaults to page name unless set earlier or on index
+$page_title = isset($page_title) ? $page_title : ucfirst(nth_last(URL['PATH_ARRAY']));
 
-if( $url === 'collection^orphans' ){
-	$url = 'collection';
+// Redirect or fail page depending on certain factors
+
+# SQL connection must be OK
+if( mysqli_connect_errno() ){
+	http_response_code(500);
+	$file = '500';
 }
-if( file_exists("views/$url.php") != 1 ){
+# file must exist
+if( file_exists("views/$file.php") != 1 ){
 	finalize('/404');
 }
-
-# Check for SQL connection
-if( mysqli_connect_errno() ){
-	#500
-	http_response_code(500);
-	$url = '500';
-}
-
-$url_split = explode('^', $url);
-$url_readable = end($url_split);
-
-if( $has_session && $url_readable === 'login'
-	|| $has_session && $url_readable === 'register'
-	) {
+# user cannot re-login
+if( in_array($file, ['login', 'register']) && $has_session ){
 	finalize('/');
 }
-
-// TODO - this is rather garbage. Better to redirect all number error codes in .htaccess to error.php or something
-if( $url_readable === '403' || $url_readable === '404' || $url_readable === '500' ){
-	$file = 'error';
-} elseif( $url_readable === 'register' ){
-	$file = 'login';
-} else {
-	$file = $url;
-}
-
 ?>
 
 <!DOCTYPE HTML>
 <html lang="en" class="t-light">
 	<head>
-		<title><?php
-			echo SITE_NAME . " - ";
-			if( $url == "index" ){
-				echo "Track Your Collections!";
-			} else {
-				echo ucfirst($url_readable);
-			}
-		?></title>
+		<title><?=SITE_NAME." - ".$page_title?></title>
 		<meta charset="utf-8">
 		<meta name="description" content="Placeholder">
 		<meta name="keywords" content="Placeholder">
@@ -113,7 +95,7 @@ if( $url_readable === '403' || $url_readable === '404' || $url_readable === '500
 				echo "page--frame ";
 			}
 
-			echo 't-'.implode(" t-", $url_split);
+			echo 't-'.implode(" t-", URL['PATH_ARRAY']);
 		?>">
 		<?php if(!isset($_GET['frame'])) : ?> 
 		<nav id="nav" class="wrapper wrapper--site-nav">
@@ -203,7 +185,7 @@ if( $url_readable === '403' || $url_readable === '404' || $url_readable === '500
 		?>
 		
 		<?php 
-		include PATH."views/$url.php";
+		include PATH."views/$file.php";
 		?>
 		
 		<?php if(!isset($_GET['frame'])) : ?> 

interface/generic.php
@@ -451,7 +451,7 @@ elseif( $action === "collection_item_create" || $action === "collection_item_edi
 		foreach( $_POST['links'] as $link ){
 			$link = trim($link);
 			if($link !== ""
-			&& filter_var($url, FILTER_VALIDATE_URL) === False
+			&& filter_var($link, FILTER_VALIDATE_URL) === False
 			&& strpos($link, 'http') === 0) {
 				$validatedLinks[] = $link;
 			}

server/server.php
@@ -62,6 +62,23 @@ function format_user_text($s) {
 	return nl2br(htmlspecialchars($s));
 }
 
+// Gets the last element of the array, starting from index 1 (last)
+function nth_last( array $arr, int $nth = 1 ){
+	return $arr[count($arr)-$nth];
+}
+
+// This function purely exists because the builtin array_filter() does not re-number array indexes
+function remove_empties( array $arr ): array {
+	$filtered_arr = [];
+	foreach( $arr as $item ){
+		if( trim($item) === '' || $item === null ){
+			continue;
+		}
+		$filtered_arr[] = $item;
+	}
+	return $filtered_arr;
+}
+
 
 
 // AUTH SYSTEM

views/collection.php
@@ -7,7 +7,7 @@ $orphaned_items = sql('
 	WHERE m.user_id=? AND c.deleted=1',
 	['i', $user['id']], false);
 
-$is_orphanage = end($external_url['path_array']) === 'orphans';
+$is_orphanage = nth_last(URL['PATH_ARRAY']) === 'orphans';
 ?>
 
 <main id="content" class="wrapper wrapper--content">

commit 50c794485310863813ee8b45b73e65d3d42a60c0

(Fix) Register page displaying as login Regression from a1356f78ba

index.php
@@ -30,9 +30,6 @@ if( empty(URL['PATH_ARRAY']) ){
 elseif( URL['PATH_ARRAY'][0] === 'collection' ){
 	$file = 'collection';
 }
-elseif( URL['PATH_ARRAY'][0] === 'register' ){
-	$file = 'login';
-}
 // TODO: this is rather garbage. Better to redirect all number error codes in .htaccess to error.php or something
 elseif( count(URL['PATH_ARRAY']) === 1 && in_array(intval(URL['PATH_ARRAY'][0]), array_keys(HttpResponse::$error_codes)) && intval(URL['PATH_ARRAY'][0]) >= 400 ){
 	$file = 'error';
@@ -42,7 +39,24 @@ else {
 	$file = str_replace('/', '^', substr(URL['PATH_STRING'], 1));
 }
 
+// Set CSS and JS files to load based on $file
+
+$static_files_to_load = [
+	'css' => [
+		'style',
+		$file
+	],
+	'js' => [
+		'scripts',
+		$file
+	]
+];
+if( count(URL['PATH_ARRAY']) > 0 && URL['PATH_ARRAY'][0] === 'register' ){
+	$static_files_to_load['css'][] = 'login';
+}
+
 // Set page title.
+
 # Defaults to page name unless set earlier or on index
 $page_title = isset($page_title) ? $page_title : ucfirst(nth_last(URL['PATH_ARRAY']));
 
@@ -73,18 +87,18 @@ if( in_array($file, ['login', 'register']) && $has_session ){
 		<link rel="icon" type="image/png" href="/static/img/favicon-32.png" sizes="32x32">
 		<link rel="icon" type="image/png" href="/static/img/favicon-256.png" sizes="256x256">
 		
-		<link rel="stylesheet" href="/static/css/style.css">
-		
-		<?php if (file_exists(PATH."static/css/".$file.".css")) : ?>
-		<link rel="stylesheet" href="<?="/static/css/".$file?>.css">
-		<?php endif ?>
-		
-		<?php if (file_exists(PATH."static/js/".$file.".js")) : ?>
-		<script type="text/javascript" src="<?="/static/js/".$file.".js"?>" defer></script>
-		<?php endif ?>
-		
-		<!--<script type="text/javascript" src="/static/js/jquery-3.3.1.min.js" async></script>-->
-		<script type="text/javascript" src="/static/js/scripts.js" defer></script>
+		<?php
+		foreach( $static_files_to_load['css'] as $css_file ){
+			if( file_exists(PATH."static/css/".$css_file.".css") ){
+				echo '<link rel="stylesheet" href="/static/css/'.$css_file.'.css">';
+			}
+		}
+		foreach( $static_files_to_load['js'] as $js_file ){
+			if( file_exists(PATH."static/js/".$js_file.".js") ){
+				echo '<script type="text/javascript" src="/static/js/'.$js_file.'.js" defer></script>';
+			}
+		}
+		?>
 		
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i|Roboto+Mono">
 		