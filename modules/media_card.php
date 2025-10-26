8a9b53a216cbd230ee7115587688d6951b47ad33
Noziro <nozirored@gmail.com>
2025-10-25 18:23:08 -0700

    (Feature) Add basic functionality to dashboard

---
+++ modules/media_card.php
@@ -0,0 +1,26 @@
+<?php /*
+This module requires:
+- A $module_media variable with the following valid media data:
+  - id
+  - user_id
+  - name
+  - image
+  - status
+  - progress
+  - episodes
+*/ ?>
+
+<div class="c-media-card" <?php if( isset($module_media['image']) ) { echo 'style="--cover: url('.$module_media['image'].')"'; } ?>>
+	<?php if( $has_session && $module_media['user_id'] === $user['id'] ) : ?>
+	<span class="c-media-card__status"><?=ucfirst($module_media['status'])?></span>
+	<div class="c-media-card__actions">
+		<a href="/item/<?=$module_media['id']?>/edit?return_to=<?=URL['PATH_STRING']?>" class="c-media-card__button">Edit</a>
+	</div>
+	<?php endif; ?>
+	<div class="c-media-card__text">
+		<a href="/item/<?=$module_media['id']?>" class="c-media-card__name"><?=$module_media['name']?></a>
+		<?php if( $has_session && isset($module_media['episodes']) && $module_media['episodes'] > 0 ) : ?>
+		<div class="c-media-card__count"><?=$module_media['progress']?> / <?=$module_media['episodes']?></div>
+		<?php endif; ?>
+	</div>
+</div>
--- views/dashboard.php
+++ views/dashboard.php
@@ -1,10 +1,28 @@
+<?php
+if( !$has_session ){
+	finalize('/403');
+}
+
+$currently_watching = sql('
+	SELECT id, user_id, status, name, image, progress, episodes
+	FROM media
+	WHERE user_id=? AND status = "current"',
+	['i', $user['id']]);
+?>
+
 <main id="content" class="wrapper wrapper--content">
 	<div class="wrapper__inner split">
 		<div class="split__section">
-			<span class="split__sidebar-header">Currently Watching / From Your Lists</span>
-			<br /><br />
-			<span class="split__sidebar-header">(Friend) Activity Feed</span>
-			<br /><br />
+			<?php if( $currently_watching->row_count > 0 ) : ?>
+			<h3 class="c-heading">From Your Lists</h3>
+			<div class="l-horizontal">
+				<?php foreach( $currently_watching->rows as $module_media ){
+					include(PATH.'modules/media_card.php');
+				}
+				endif; ?>
+			</div>
+
+			<h3 class="c-heading">(Friend) Activity Feed</h3>
 		</div>
 	</div>
 </main>
